47e75c729230bcf3b7480f4fb21dff489aade837
Komail Kanjee <1796298+KomailKanjee@users.noreply.github.com>
2024-05-02 09:04:16 -0400

    update docs and examples

--- examples/basic-deploy-local/main.tf
+++ examples/basic-deploy-local/main.tf
@@ -1,15 +1,14 @@
 module "acme_certificates" {
-  source                             = "github.com/komailio/terraform-acme-certificates"
-  acme_registration_account_key_pems = tls_private_key.acme_registration_private_key.private_key_pem
-  acme_registration_email            = "noreply@example.com"
+  source                            = "komailo/acme-certificates/modules"
+  acme_registration_account_key_pem = tls_private_key.acme_registration_private_key.private_key_pem
+  acme_registration_email           = "noreply@example.com"
 
   certificate_signing_requests = local.certificate_signing_requests
 
   deploy_local_file_path = "${path.root}/deploy_local_file"
 
-  dns_challenge = [
-    {
-      provider = "dynu"
+  dns_challenges = {
+    dynu = {
       config = {
         DYNU_API_KEY             = "my-api-key"
         DYNU_HTTP_TIMEOUT        = "10"
@@ -18,5 +17,5 @@ module "acme_certificates" {
         DYNU_TTL                 = "60"
       }
     }
-  ]
+  }
 }
